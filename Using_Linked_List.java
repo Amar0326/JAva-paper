0c92ba9a379067f806a1a8c94c14f66b15837c4f
Amar Jadhav <103642714+Amar0326@users.noreply.github.com>
2022-12-12 14:06:27 +0530

    Add files via upload

---
+++ Using_Linked_List.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Using_Linked_List {
+
+  public static void main(String[] args) {
+    LinkedList<String> Name = new LinkedList<String>();
+    Name.add("Amar");
+    Name.add("Yash");
+    Name.add("Shivraj");
+    Name.add("Atharv");
+    Name.add("Zadpi");
+
+    Name.addFirst("Unstopable");
+    Name.addLast("Group");
+    Name.remove(05);
+    System.out.println(Name);
+  }
+}
