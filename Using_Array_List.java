a8a4226cf6132d1322cb598a24d5fb092f69e86e
Amar Jadhav <103642714+Amar0326@users.noreply.github.com>
2022-12-12 13:54:19 +0530

    Add files via upload

---
+++ Using_Array_List.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Using_Array_List {
+    public static void main(String[] args) {
+        ArrayList<String> Name = new ArrayList<String>();
+        Name.add("Amar");
+        Name.add("Yash");
+        Name.add("Shivraj");
+        Name.add("Atharv");
+        Name.add("Dalvi");
+        Name.remove(04);
+        System.out.println(Name);
+        
+
+    }
+}
